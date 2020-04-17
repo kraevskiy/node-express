c1c3dee64ee43514c0a86770ecd2f02b9edcb484
kraevskiy <ilya.labelplatform@gmail.com>
2020-04-17 21:32:58 +0300

    add courses list

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,13 @@
 const {Router} = require('express')
+const Course = require('../models/course')
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const courses = await Course.getAll()
   res.render('courses', {
     title: 'List',
-    isCourses: true
+    isCourses: true,
+    courses
   })
 })
 
