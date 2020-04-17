ed8b32aa3c274f071acef2750e880053ffdf0335
kraevskiy <ilya.labelplatform@gmail.com>
2020-04-17 19:51:41 +0300

    add title for page && add active nav for navbar

--- index.js
+++ index.js
@@ -16,11 +16,24 @@ app.set('views', 'views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Main page',
+    isHome: true
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.render('about')
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'New course',
+    isAdd: true
+  })
+})
+
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'List',
+    isCourses: true
+  })
 })
 
 const PORT = process.env.POST || 3000
