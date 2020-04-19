1b06f3ac4b4983ca7aad8f5f954d1a5e761f4822
kraevskiy <ilya.labelplatform@gmail.com>
2020-04-19 17:32:27 +0300

    Add registration

--- routes/auth.js
+++ routes/auth.js
@@ -20,12 +20,30 @@ router.post('/login', async (req, res) => {
   req.session.user = user
   req.session.isAuthenticated = true
   req.session.save(err => {
-    if(err){
+    if (err) {
       throw err
     } else {
       res.redirect('/')
     }
   })
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const {email, password, repeat, name} = req.body
+    const candidate = await User.findOne({email})
+    if (candidate) {
+      res.redirect('/auth/login#register')
+    } else {
+      const user = new User({
+        email, name, password, cart: {items:[]}
+      })
+      await user.save()
+      res.redirect('/auth/login#login')
+    }
+  } catch (e) {
+    console.log(e);
+  }
 })
 
 module.exports = router
