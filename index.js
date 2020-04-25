15e17f02cecab765841f0ce0c8e31c857544faa3
kraevskiy <ilya.labelplatform@gmail.com>
2020-04-25 12:17:35 +0300

    Add prod dev Node_ENV

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/images',express.static(path.join(__dirname, 'images')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: keys.SESSION_SECRET,
---
+++ keys/keys.dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://rambutan:tvmz6OECZbQzeASN@cluster0-mhhnb.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value',
+  // SENDGRID_API_KEY: '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',
+  SENDGRID_API_KEY: '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',
+  EMAIL_FROM: 'hello@rambutan.site',
+  BASE_URL: 'http://localhost:3000'
+}
