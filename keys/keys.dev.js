b1e9718d4f07371de5e7cc09df6bf7d9920b4366
kraevskiy <ilya.labelplatform@gmail.com>
2020-05-02 22:51:13 +0300

    create env security keys

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 module.exports = {
-  MONGODB_URI: 'mongodb+srv://rambutan:tvmz6OECZbQzeASN@cluster0-mhhnb.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value',
-  // SENDGRID_API_KEY: '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',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'hello@rambutan.site',
-  BASE_URL: 'http://localhost:3000'
+  MONGODB_URI: process.env.MONGODB_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  BASE_URL: process.env.BASE_URL
 }
--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,7 +1,6 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   SESSION_SECRET: process.env.SESSION_SECRET,
-  // SENDGRID_API_KEY: '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',
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   EMAIL_FROM: process.env.EMAIL_FROM,
   BASE_URL: process.env.BASE_URL
